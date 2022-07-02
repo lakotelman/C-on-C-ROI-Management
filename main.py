93bb6593611d3e0dc74fc7f8fd5438329649be24
lakotelman <la.n.kot@pm.me>
2022-07-01 20:09:16 -0800

    edit users, edit properties, display properties

--- main.py
+++ main.py
@@ -1,119 +1,247 @@
+from attr import attrib
+
+
 class User:
     def __init__(self, profile_name):
         self.profile_name = profile_name
         self.portfolio = {}
 
+    def display_portfolio(self):
+        for name, prop_obj in self.portfolio.items():
+            print(
+                f"""\n{name.title()}
+    -----------------------------------------
+    Initial Investment: {prop_obj.investment}
+    Monthly Gross Income: {prop_obj.gross}
+    Monthly Expenses: {prop_obj.expenses}
+    Montly Net Income: {prop_obj.net}
+    ==========================================
+    Cash on Cash Return on Investment: %{prop_obj.roi}\n"""
+            )
+
+
 t1 = User("mr.monopoly")
 t2 = User("mr.green")
 
-class AppControl: 
+
+class AppControl:
     def __init__(self):
-        self.all_users = {t1.profile_name: t1,
-                        t2.profile_name: t2 }
-        self.active_user = self.all_users["mr.monopoly"]
-    
-    def control_create_user(self, user): 
+        self.all_users = {t1.profile_name: t1, t2.profile_name: t2}
+        self.active_user = t1
+
+    def control_create_user(self, user):
         self.all_users[user.profile_name] = user
-    
+
     def control_toggle_user(self, user_obj):
         self.active_user = user_obj
-        
-    def control_edit_user(self): 
-        pass 
+
+    def control_edit_user(self, user_obj, option, new_name=None):
+        if option == "delete":
+            if user_obj is self.active_user:
+                self.active_user = None
+            del self.all_users[user_obj.profile_name]
+        if option == "rename" and new_name != None:
+            user_obj.profile_name = new_name
 
     def control_add_property(self, property):
-        self.all_users[self.active_user.profile_name].portfolio[property.address] = property
+        self.all_users[self.active_user.profile_name].portfolio[
+            property.address
+        ] = property
         return self.all_users[self.active_user.profile_name]
 
-    def control_edit_property(self): 
-        pass 
-
+    def control_edit_property(
+        self,
+        property,
+        option,
+        *,
+        upd_address=None,
+        upd_gross=None,
+        upd_expenses=None,
+        upd_investment=None,
+    ):
+        if option == "revise":
+            if upd_address is not None:
+                property.address = upd_address
+            if upd_gross is not None:
+                property.gross = int(upd_gross)
+            if upd_expenses is not None:
+                property.expenses = int(upd_expenses)
+            if upd_investment is not None:
+                property.investment = int(upd_investment)
+        if option == "delete":
+            del self.active_user.portfolio[property.address]
 
 
 class AppView:
     def __init__(self, controller):
-        self.controller = controller 
-        
+        self.controller = controller
+
     def view_create_user(self):
         profile_name = input("What would you like your account name to be? ")
-        u = User(profile_name)
-        self.controller.add_user_to_all(u)
-        print(f"Added {profile_name} to the co-op account")
-        self.run()
+        u = User(profile_name.strip().lower())
+        self.controller.control_create_user(u)
+        print(f"\nAdded {profile_name} to the co-op account")
+        return
 
     def view_toggle_user(self):
         choice_dict = {}
         for i, profile_name in enumerate(self.controller.all_users.items()):
-            print(f"[{i+1}] {profile_name[0]}")
-            choice_dict[str(i+1)] = profile_name[1]
+            print(f"[{i+1}] {profile_name[0].title()}")
+            choice_dict[str(i + 1)] = profile_name[1]
         toggle = input("\nWhich profile would you like to view? ")
         try:
             self.controller.control_toggle_user(choice_dict[toggle])
-            print(f"\nActive User is {self.controller.active_user.profile_name.title()}")
-            self.run()
-        except Exception as err: 
+            print(f"Switched user to {choice_dict[toggle].profile_name.title()}")
+            return
+        except Exception as err:
             print("\nInvalid input. Please try again")
             self.view_toggle_user()
 
     def view_edit_user(self):
-        pass
+        choice_dict = {}
+        for i, profile_name in enumerate(self.controller.all_users.items()):
+            print(f"[{i+1}] {profile_name[0].title()}")
+            choice_dict[str(i + 1)] = profile_name[1]
+        toggle = input("\nWhich profile would you like to edit? ")
+        print("Which would you like to do?")
+        option = input("\n[1] Change profile name? [2] Delete Profile? ")
+        if option == "2":
+            print(
+                "Are you sure? This is permanent and your profile cannot be retrieved again after deleting. Type your profile name to confirm"
+            )
+            confirm = input("")
+            if confirm.lower() == choice_dict[toggle].profile_name:
+                self.controller.control_edit_user(choice_dict[toggle], "delete")
+            print(f"{choice_dict[toggle]} was deleted")
+            return
+
+        if option == "1":
+            new_name = input("What would you like to change your name to?")
+            self.controller.control_edit_user(choice_dict[toggle], "rename", new_name)
 
     def display_portfolio(self):
-        pass
+        if not self.controller.active_user.portfolio:
+            print("Your portfolio is currently empty. Add some properties!")
+        else:
+            self.controller.active_user.display_portfolio()
 
     def view_new_property(self):
         address = input("\nWhat is the address for this property? ")
-        investment = input("\nFor this property, what is the total for your initial investment? ")
-        gross = input("\n For this property, what is the total for your monthly gross income? ")
+        investment = input(
+            "\nFor this property, what is the total for your initial investment? "
+        )
+        gross = input(
+            "\nFor this property, what is the total for your monthly gross income? "
+        )
         expenses = input("\n For this property, what is your total monthly expenses? ")
-        p = Property(address, int(gross), int(expenses), int(investment))
-        self.controller.add_property_to_portfolio(p)
-        print(self.controller.active_user.profile_name)
-        print(self.controller.active_user.portfolio)
-        self.run()
+        p = Property(address.lower(), int(gross), int(expenses), int(investment))
+        self.controller.control_add_property(p)
+        print(
+            f"\nExcellent, we added {p.address.title()} to your investment portfolio. You are getting a %{p.roi} return on investment. Neat.\n"
+        )
 
     def view_edit_property(self):
-        pass
+        if not self.controller.active_user:
+            print("Your portfolio is currently empty. Add some properties!")
+            return
+        choice_dict = {}
+        for i, property in enumerate(self.controller.active_user.portfolio.items()):
+            print(f"[{i+1}] {property[0].title()}")
+            choice_dict[str(i + 1)] = property[1]
+        toggle = input("\nWhich property would you like to edit? ")
+        print("What would you like to do?")
+        option = "[1] Change details\n [2] Delete the property from your portfolio? "
+        if option == "2":
+            print("Are you sure? This cannot be undone ")
+            check = input("Type the address of your property to confirm ")
+            if check.lower() == choice_dict[toggle].address:
+                self.controller.control_edit_property(choice_dict[toggle], "delete")
+        if option == "1":
+            editing = True
+            while editing == True:
+                print("Which detail would you like to change?")
+                edit = input("[1]Address [2]Investment [3]Expenses [4]Income")
+                revision = input("What is your new value? ")
+                if edit == "1":
+                    self.controller.control_edit_property(
+                        choice_dict[toggle], "revise", upd_address=revision
+                    )
+                    print(f"Updated {choice_dict[toggle]} address")
+                if edit == "2":
+                    self.controller.control_edit_property(
+                        choice_dict[toggle], "revise", upd_investment=revision
+                    )
+                    print(f"Updated {choice_dict[toggle]} investment")
+                if edit == "3":
+                    self.controller.control_edit_property(
+                        choice_dict[toggle], "revise", upd_expenses=revision
+                    )
+                    print(f"Updated {choice_dict[toggle]} expenses")
+                if edit == "4":
+                    self.controller.control_edit_property(
+                        choice_dict[toggle], "revise", upd_income=revision
+                    )
+                    print(f"Updated {choice_dict[toggle]} income")
+                loop = "Would you like to make more revisions to this property? [Y/N] "
+                if loop.lower() == "n":
+                    break
+                    editing = False
+                if loop.lower() == "y":
+                    continue
 
     def run(self):
-        print(f"Welcome, {self.controller.active_user.profile_name.title()}")
-        print("What would you like to do?")
-        print("""
+        if self.controller.active_user:
+            print(f"Active User: {self.controller.active_user.profile_name.title()}")
+        run = True
+        while run == True:
+            if self.controller.all_users is False:
+                print("Let's add a user to get you started!\n")
+                self.view_create_user()
+            if self.controller.active_user is None:
+                print(
+                    "You currently don't have an active user chosen. Please set one\n"
+                )
+                self.view_toggle_user()
+            print("\nWhat would you like to do?")
+            print(
+                """
 [1] Change Profile
 [2] Add Profile
-[3] Edit Profile
-[4] View your portfolio and properties 
-[5] Create a new property
+[3] Edit Profiles
+[4] View your portfolio
+[5] Add a new property to your portfolio
 [6] Edit your portfolio
 [7] Quit
-        """)
-        while True:
-            menu_select = input("") 
-            if menu_select == "1": 
+        """
+            )
+            menu_select = input("")
+            if menu_select == "1":
                 self.view_toggle_user()
-            if menu_select == "2": 
+            elif menu_select == "2":
                 self.view_create_user()
-            if menu_select == "3": 
-                pass
-            if menu_select =="4": 
-                pass 
-            if menu_select == "5": 
+            elif menu_select == "3":
+                self.view_edit_user()
+            elif menu_select == "4":
+                self.display_portfolio()
+            elif menu_select == "5":
                 print("Just a few questions...")
                 self.view_new_property()
-            if menu_select == "6": 
+            elif menu_select == "6":
                 self.view_edit_property()
-            if menu_select == "7": 
+            elif menu_select == "7":
+                print("Cash money! Have a nice day!")
+                run = False
                 break
 
 
-class Property: 
-    def __init__(self, address, gross, expenses, investment): 
-        self.address = address 
+class Property:
+    def __init__(self, address, gross, expenses, investment):
+        self.address = address
         self.gross = gross
-        self.expenses = expenses 
+        self.expenses = expenses
         self.investment = investment
         self.net = self.gross - self.expenses
-        self.roi =int(((self.net *12)/self.investment) * 100)
+        self.roi = int(((self.net * 12) / self.investment) * 100)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -2,33 +2,64 @@ from main import User, AppControl, t1, AppView, Property
 import unittest
 
 
-class TestControls(unittest.TestCase): 
+class TestControls(unittest.TestCase):
     def test_control_create_user(self):
         a = AppControl()
-        u = User("Sam Gamgee") 
+        u = User("Sam Gamgee")
         a.all_users[u.profile_name] = u
         self.assertIn(u.profile_name, a.all_users)
 
-    def test_control_toggle_user(self): 
+    def test_control_toggle_user(self):
         a = AppControl()
         u = User("Bilbo Baggins")
         a.control_create_user(u)
         a.control_toggle_user(u)
         self.assertEqual(a.active_user, u)
-    
+
+    def test_control_edit_user_delete(self):
+        a = AppControl()
+        a.control_edit_user(t1, "delete")
+        self.assertEqual(a.active_user, None)
+        self.assertNotIn(t1, a.all_users)
+
+    def test_control_edit_user_rename(self):
+        a = AppControl()
+        a.control_edit_user(t1, "rename", "billybob")
+        self.assertEqual(t1.profile_name, "billybob")
+
     def test_control_add_property(self):
         pass
 
+    def test_control_edit_property(self):
+        a = AppControl()
+        p = Property("4242 Bag End Ln", 1000, 800, 10000)
+        a.control_edit_property(
+            p,
+            "revise",
+            upd_address="4243 Bag End Ln",
+            upd_expenses="1000",
+            upd_gross="1000",
+            upd_investment="12000",
+        )
+        self.assertEqual(p.address, "4243 Bag End Ln")
+        self.assertEqual(p.expenses, 1000)
+        self.assertEqual(p.gross, 1000)
+        self.assertEqual(p.investment, 12000)
+
+    def test_control_edit_property_delete(self):
+        u = User("Bilbo Baggins")
+        a = AppControl()
+        p = Property("4242 Bag End Ln", 1000, 800, 10000)
+        a.control_add_property(p)
+        a.control_edit_property(p, "delete")
+        self.assertNotIn(p, u.portfolio)
 
 class TestProperty(unittest.TestCase):
-    def test_property_init(self): 
+    def test_property_init(self):
         p = Property("3145 Bag End Ln", 1000, 800, 10000)
         self.assertEqual(p.net, 200)
         self.assertEqual(p.roi, 24)
 
 
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     unittest.main()
-
-
